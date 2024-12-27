ea340bbd3e63877d204b471a7e4ccc9b41f3815a
Javier <javiertrejorod@gmail.com>
2024-12-27 02:10:07 -0600

    config: set conmfiguration to export project

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: "/people-and-cats",
+  trailingSlash: true,
 };
 
 export default nextConfig;
